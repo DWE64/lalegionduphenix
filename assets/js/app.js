145573533e75339f4d7a102746f6c6b4661606a0
dwe64 <developpement.web.entreprise64@gmail.com>
2019-09-06 01:38:21 +0200

    creation formulaire inscription+connexion

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,10 +5,29 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
+// require the JavaScript
+require('bootstrap-star-rating');
+
+
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.css');
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-svg/theme.css');
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
-// const $ = require('jquery');
+const $ = require('jquery');
+
+//this "modifies" the jquery module: adding behavior to it
+//the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+//or you can include specific pieces
+//require('bootstrap/js/dist/tooltip');
+//require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+ $('[data-toggle="popover"]').popover();
+});
+
 
 console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
